52d5ebc33499bbe8e622ae1a7c08e687e480b697
modengann <99417989+modengann@users.noreply.github.com>
2025-12-16 20:11:59 -0500

    init

---
+++ src/App.java
@@ -0,0 +1,12 @@
+import java.nio.file.Paths;
+import java.util.*;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        String fileName = "nfl_team_stats_2003_2023.csv";
+
+       
+       
+       
+    }
+}
